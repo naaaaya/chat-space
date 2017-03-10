a416a8795478430a506c0e5b02c3be7b0cfd82be
naaya saito <naaya.dance@gmail.com>
2017-03-10 14:53:18 +0900

    Change hash notation into symbol notation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  has_many :groups, through: :users_groups
+has_many :users_groups
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
 end
