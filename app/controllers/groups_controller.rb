4fd8c1b016f28cff5352bff2acb4d553402e4ed1
naaya saito <naaya.dance@gmail.com>
2017-03-11 21:04:04 +0900

    create edit function

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -19,9 +19,13 @@ class GroupsController < ApplicationController
   end
 
   def edit
+    @group = Group.find(params[:id])
   end
 
   def update
+    @group = Group.find(params[:id])
+    @group.update(group_params)
+    redirect_to root_path
   end
 
   private
