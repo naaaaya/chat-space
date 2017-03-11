96cced5fd8c966096778dcca87b97d5e2d5e15b9
naaya saito <naaya.dance@gmail.com>
2017-03-11 18:34:27 +0900

    Fixed model name

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,5 +1,5 @@
 class Group < ApplicationRecord
-  has_many :users,through: :users_groups
-  has_many :users_groups
+  has_many :users,through: :users_group
+  has_many :users_group
   accepts_nested_attributes_for :users
 end
