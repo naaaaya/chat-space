9c8b973fee8925cedd7aea93f7138084e273d21d
naaya saito <naaya.dance@gmail.com>
2017-03-11 20:02:02 +0900

    Write create function

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,11 +1,36 @@
 class GroupsController < ApplicationController
   def new
     @group = Group.new
+    @group.users.build
+  end
+
+  def create
+    @users =[]
+    group = Group.create(group_params)
+    user_ids = user_params[:user_ids]
+    user_ids.each do|user_id|
+      @users << User.find(user_id) if user_id
+    end
+    @users.each do |user|
+      user.groups << group
+    end
+
+    redirect_to root_path
   end
 
   def edit
   end
 
   def update
+  end
+
+  private
+
+  def user_params
+    params.require(:group).permit(user_ids:[])
+  end
+
+  def group_params
+    params.require(:chat_group).permit(:name)
   end
 end
--- app/models/group.rb
+++ app/models/group.rb
@@ -1,5 +1,4 @@
 class Group < ApplicationRecord
   has_many :users,through: :users_group
   has_many :users_group
-  accepts_nested_attributes_for :users
 end
