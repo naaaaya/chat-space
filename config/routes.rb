2b5c30e29ad59a1640fe1e28669c0347e951e07a
naaya saito <naaya.dance@gmail.com>
2017-03-12 17:42:37 +0900

    Nest groups and messages

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'top#index'
-  resources :groups
+  resources :groups do
+    resources :messages, only: :index
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
