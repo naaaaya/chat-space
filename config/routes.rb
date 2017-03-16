f33d4083b48c1b5fcc0cc1260fe9c94780f55788
naaya saito <naaya.dance@gmail.com>
2017-03-17 08:29:23 +0900

    Set resource for message#create

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root 'groups#index'
   resources :groups do
-    resources :messages, only: :index
+    resources :messages, only: :create
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
