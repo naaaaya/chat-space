1b37f7dd21b1b8b836b11c2830ed05c85e284812
naaya saito <naaya.dance@gmail.com>
2017-03-17 09:47:38 +0900

    Add message-send-function

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,5 +1,14 @@
 class MessagesController < ApplicationController
+
   def create
-    
+    Message.create(message_params)
+    redirect_to group_path(id:params[:group_id])
   end
+
+  private
+
+  def message_params
+    params.permit(:body,:group_id).merge(user_id: current_user.id)
+  end
+
 end
