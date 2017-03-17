6067cf3714361de7e3d57c99e5a37f8f9d278ce4
naaya saito <naaya.dance@gmail.com>
2017-03-17 17:15:16 +0900

    Add group_params method

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,7 +2,7 @@ class MessagesController < ApplicationController
 
   def create
     Message.create(message_params)
-    redirect_to group_path(id:params[:group_id])
+    redirect_to group_path(group_params)
   end
 
   private
@@ -11,4 +11,7 @@ class MessagesController < ApplicationController
     params.permit(:body,:group_id).merge(user_id: current_user.id)
   end
 
+  def group_params
+    @group = Group.find(params[:group_id])
+  end
 end
