916b16ff076f274ab8a3845e685f18e4aca62f6c
naaya saito <naaya.dance@gmail.com>
2017-03-18 17:03:01 +0900

    Add blank and indent

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
-    def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-    end
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
 
-    def defines_groups_messages_variables
+  def defines_groups_messages_variables
     @group = Group.find(params[:group_id])
     @groups = current_user.groups
     @messages =@group.messages
-    end
+  end
 end
