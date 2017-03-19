f4a175427ad2a8e740f6b89a53f49f2eccb340af
naaya saito <naaya.dance@gmail.com>
2017-03-19 15:42:37 +0900

    Set faker to group, user, and message

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,11 +1,17 @@
 FactoryGirl.define do
+  pass = Faker::Internet.password(8)
 
   factory :user do
-    name Faker::Name.name
-    email Faker::Internet.email
-    password = Faker::Internet.password(8)
-    password password
-    password_confirmation password
-  end
+    name                   Faker::Name.name
+    email                  Faker::Internet.email
+    password               pass
+    password_confirmation  pass
 
+    after(:create) do |user|
+       temp_group = create(:group)
+      # ここでgroupをcreateしてtemp_groupに入れる
+      create(:users_group, user: user, group: temp_group)
+
+    end
+  end
 end
