33e61b0a096af0001b41111f9596c53165266c2c
naaya saito <naaya.dance@gmail.com>
2017-03-26 10:54:31 +0900

    Append message to html

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,11 +1,17 @@
 $(function(){
-  $('.chat-view__form .send-button').on('submit', function(e) {
+
+  function buildHTML(message) {
+    var html = $('<li class= "message">').append(message.content);
+    return html;
+  }
+
+  $('.chat-view__form .send-button').on('click', function(e) {
     e.preventDefault();
     var textField = $('.message-text-field');
     var message = textField.val();
     $.ajax({
       type: 'POST',
-      url: group_messages_path,
+      url: '/messages.json',
       data: {
         message: {
           content: message
@@ -14,11 +20,13 @@ $(function(){
       dataType: 'json'
     })
     .done(function(data) {
+      var html = buildHTML(data);
+      $('.chat-box').append(html);
       textField.val('');
     })
     .fail(function() {
       alert('error');
     });
+    return false;
   });
 });
-
