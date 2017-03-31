dbce3dc6be6b1988c3c492d8f2d6b8bbe1f4c3f4
naaya saito <naaya.dance@gmail.com>
2017-03-31 18:15:53 +0900

    Modified appendAddedList and appendSearchedList into literal notation

--- app/assets/javascripts/group.js
+++ app/assets/javascripts/group.js
@@ -5,19 +5,19 @@ $(function(){
   var add_users = []
 
   function appendSearchedList(user, index) {
-    var item = $('<li class="chat-group-user searched_name" id ='+ index +'>').append(user.name + '<p class = "chat-group-user__btn chat-group-user__btn--add">追加');
+    var item = $(`<li class="chat-group-user searched_name" id =${index}>`).append(`${user.name}<p class = "chat-group-user__btn chat-group-user__btn--add">追加` );
     result.append(item);
   };
 
   function appendAddedList(user, index) {
-    var item = $('<li class="chat-group-user chat-group-user__name", id ='+ index +'>').append(user.name + '<p class = "chat-group-user__btn chat-group-user__btn--remove">削除');
+    var item = $(`<li class="chat-group-user chat-group-user__name", id =${index}>`).append(`${user.name} <p class = "chat-group-user__btn chat-group-user__btn--remove">削除`);
     $("#chat-group-users").append(item);
   };
 
 
   $('#chat_group_member').on('keyup', function() {
     var input = $(this).val();
-    if (preinput != input ){
+    if (preinput != input){
     $('.searched_name').remove();
     $.ajax({
       type: 'GET',
@@ -48,7 +48,6 @@ $(function(){
     add_users.push(user.id);
      $("#hiddenID").val(add_users);
     $('.searched_name').remove();
-    console.log(add_users);
   });
 
   $(document).on('click', '.chat-group-user__btn--remove',  function() {
@@ -56,11 +55,8 @@ $(function(){
     delete_user = users[user_id];
     add_users.some(function(value, i){
       if (value == delete_user.id) add_users.splice(i,1);
-      console.log('deleted');
     });
     $(this).parent().remove();
-    console.log(add_users);
-
   });
 
 
