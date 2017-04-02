b25c5ecc077848bba55099227ef14f30dd43d08a
naaya saito <naaya.dance@gmail.com>
2017-04-02 12:58:56 +0900

    Use data property

--- app/assets/javascripts/group.js
+++ app/assets/javascripts/group.js
@@ -4,13 +4,13 @@ $(function(){
   var preinput = ''
   var add_users = []
 
-  function appendSearchedList(user, index) {
-    var item = $(`<li class="chat-group-user searched_name" id =${index}>`).append(`${user.name}<p class = "chat-group-user__btn chat-group-user__btn--add">追加` );
+  function appendSearchedList(user) {
+    var item = $(`<li class="chat-group-user searched_name", data-user-id = ${user.id}, data-user-name = ${user.name} > ${user.name} <p class = "chat-group-user__btn chat-group-user__btn--add">追加</p></li>` );
     result.append(item);
   };
 
-  function appendAddedList(user, index) {
-    var item = $(`<li class="chat-group-user chat-group-user__name", id =${index}>`).append(`${user.name} <p class = "chat-group-user__btn chat-group-user__btn--remove">削除`);
+  function appendAddedList(name) {
+    var item = $(`<li class="chat-group-user chat-group-user__name",data-user-name = ${name}> ${name} <p class = "chat-group-user__btn chat-group-user__btn--remove">削除</p></li>`);
     $("#chat-group-users").append(item);
   };
 
@@ -18,45 +18,46 @@ $(function(){
   $('#chat_group_member').on('keyup', function() {
     var input = $(this).val();
     if (preinput != input){
-    $('.searched_name').remove();
-    $.ajax({
-      type: 'GET',
-      url: '/users.json',
-      data:{
-        keyword: input
-      },
-      dataType: 'json'
-    })
-    .done(function(data) {
-      users = data
-      $.each(users, function(i, user){
-        appendSearchedList(user,i);
+      $('.searched_name').remove();
+      $.ajax({
+        type: 'GET',
+        url: '/users.json',
+        data:{
+          keyword: input
+        },
+        dataType: 'json'
+      })
+      .done(function(data) {
+        users = data
+        $.each(users, function(i, user){
+          appendSearchedList(user);
+        });
+      })
+      .fail(function() {
+        alert('error');
       });
-    })
-    .fail(function() {
-      alert('error');
-    });
-  preinput = input
-  };
+      preinput = input
+    };
   });
 
 
   $(document).on('click', '.chat-group-user__btn--add', function() {
-    user_id = $(this).parent().attr('id');
-    user = users[user_id];
-    appendAddedList(user, user_id);
-    add_users.push(user.id);
-     $("#hiddenID").val(add_users);
+    var list = $(this).parents()
+    user_id = list.data('userId');
+    user_name = list.data('userName');
+    appendAddedList(user_name);
+    add_users.push(user_id);
+    $("#hiddenID").val(add_users);
     $('.searched_name').remove();
   });
 
   $(document).on('click', '.chat-group-user__btn--remove',  function() {
-    user_id = $(this).parent().attr('id');
-    delete_user = users[user_id];
+    var list = $(this).parents()
+    user_id = button.data('userId');
     add_users.some(function(value, i){
-      if (value == delete_user.id) add_users.splice(i,1);
+      if (value == user_id) add_users.splice(i,1);
     });
-    $(this).parent().remove();
+    list.remove();
   });
 
 
